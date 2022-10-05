183b598bb37f0b13886a4b6f8cf59120ecbc6c7a
Flagelo6482 <frankln6482@gmail.com>
2022-10-04 19:24:44 -0700

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Ejecutarmos el abecedario en
+ *        minusculas y mayusculas.
+ * Return: Retornaremos 0.
+ */
+int main(void)
+{
+	int mh;
+        int ch;
+
+        for (ch = 'a'; ch <= 'z'; ch++)
+                putchar (ch);
+	for (mh = 'A'; mh <= 'Z'; mh++)
+		putchar (mh);
+	putchar ('\n');
+        return (0);
+}
