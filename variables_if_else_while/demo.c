bc61a587888838c6a790240bd6aecb7c7759d081
Flagelo6482 <frankln6482@gmail.com>
2022-10-04 17:25:30 -0700

    Positive anything is better than negative nothing

---
+++ variables_if_else_while/demo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n=100;
+
+	if(n == 10)
+	{
+		printf("El valor es 10 \n");
+	}
+	else if(n == 20)
+	{
+		printf("El valor es 20\n");
+	}
+	else
+	{
+		printf("Otro valor \n");
+	}
+}
