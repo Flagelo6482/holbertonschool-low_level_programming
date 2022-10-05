06c46ba8930b92dfdb45b0fd1a461e0b6586fa13
Flagelo6482 <frankln6482@gmail.com>
2022-10-04 20:08:42 -0700

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Ejecutamos los numeros del 0 al 10.
+ * Return: Retornamos 0.
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		printf("%d", a);
+		a++;
+	}
+	printf("\n");
+	return (0);
+}
