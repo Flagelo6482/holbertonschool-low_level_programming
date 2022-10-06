bddce1f08dea60f81e4ddad5462b38859d53a64a
Flagelo6482 <frankln6482@gmail.com>
2022-10-05 18:46:17 -0700

    Hexadecimal

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (y = 'a'; y <= 'f'; y++)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
