a2e63db77bb48a737c6cd7f57f53c3e522ca6026
Flagelo6482 <frankln6482@gmail.com>
2022-10-06 17:12:57 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Ejecutaremos numeros seguidos de "," y " ".
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar((a%10) + '0');
+		if (a == 9)
+			continue;
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return(0);
+}
