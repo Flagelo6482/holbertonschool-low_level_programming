118353db086ac0f156c3d9a0ff9d2b69cd830314
Flagelo6482 <frankln6482@gmail.com>
2022-10-06 17:16:37 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,12 +11,12 @@ int main(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		putchar((a%10) + '0');
+		putchar((a % 10) + '0');
 		if (a == 9)
 			continue;
 		putchar(44);
 		putchar(32);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
