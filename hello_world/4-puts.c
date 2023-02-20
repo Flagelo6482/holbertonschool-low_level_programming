68c5749964dfd41d5ec929020cbff472ebd4aaf6
Flagelo6482 <frankln6482@gmail.com>
2023-02-20 12:49:09 -0800

    feat: Adding

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ *main: void
+ *@puts: Imprime la cadena especificada,incluido un salto de linea
+ *Return: 0
+ */
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
