8e5c89c48543139fbcc78c8de599cbddc458b352
Flagelo6482 <frankln6482@gmail.com>
2023-02-20 16:24:22 -0800

    feat:updated

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- *main: void
+ *main - Entry point
  *@puts: Imprime la cadena especificada,incluido un salto de linea
- *Return: 0
+ *Return: Always 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
