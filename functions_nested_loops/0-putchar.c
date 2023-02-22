707f85ab16e6ef8ef52ba046fc4e49539a2bfdd4
Flagelo6482 <frankln6482@gmail.com>
2023-02-22 15:48:48 -0800

    feat: Update

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  *main - Entry point
- *@c: Print character for character
+ *
  *Return: Always 0
  */
 int main(void)
 {
 	int x;
 
-	char nombre[]="_putchar";
+	char nombre[] = "_putchar";
 
 	for (x = 0; x <= 8; x++)
 	{
