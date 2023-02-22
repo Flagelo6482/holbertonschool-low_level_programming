bc015dabb3e3bd8d228edbfd82150503835532f7
Flagelo6482 <frankln6482@gmail.com>
2023-02-21 18:44:18 -0800

    feat:Prints Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Print from 0 to 10
+ *
+ *Return:Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+
+	printf("\n");
+	return (0);
+}
