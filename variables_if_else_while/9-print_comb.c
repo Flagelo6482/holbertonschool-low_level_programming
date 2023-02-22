c3f32d099f49b8ff8ff922d80ae6a6cfb93f2deb
Flagelo6482 <frankln6482@gmail.com>
2023-02-21 19:34:04 -0800

    feat:using break

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ *main - Using break
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+
+		if (x == '9')
+		{
+			break;
+		}
+
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
