fd95f056b9fd27e092f1902a1be077886a2f15b9
Flagelo6482 <frankln6482@gmail.com>
2023-02-23 20:02:00 -0800

    feat: A + B

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *add - Return result that adds two integers
+ *@a: First variable
+ *@b: Second variabe
+ *
+ *Return: Adds thow integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
