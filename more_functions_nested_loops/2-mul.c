798bbce20c1c0fbcf435c1dea11bec6edb956780
Flagelo6482 <frankln6482@gmail.com>
2023-02-25 06:17:55 -0800

    feat: Multiplies two integers

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - Multiplies two integers
+ *@a: Var1
+ *@b: Var2
+ *Return: Result
+ */
+int mul(int a, int b)
+{
+	int mult;
+	
+	mult = a * b;
+
+	return (mult);
+}
