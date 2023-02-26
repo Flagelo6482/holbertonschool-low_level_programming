badb6843e7ef47a31c1d6b0dfd67d712d202d148
Flagelo6482 <frankln6482@gmail.com>
2023-02-26 04:05:09 -0800

    feat:FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Print Fizz, Buzz and FizzBuzz
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
+			printf("FizzBuzz ");
+
+		else if (x % 3 == 0)
+			printf("Fizz ");
+
+		else if (x % 5 == 0)
+			printf("Buzz ");
+
+		else
+			printf("%d ", x);
+	}
+
+	putchar(10);
+
+	return (0);
+}
