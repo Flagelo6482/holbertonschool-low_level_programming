e1d74cbd9bc8b127d88cabbf07cb6548ebf7ddde
Flagelo6482 <frankln6482@gmail.com>
2023-03-01 16:40:47 -0800

    feat: Prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_rev - Print a string in reverse, followed by a new line
+ *@s: Pointer
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	len = len - 1;
+
+	while (s[len] >= 0)
+	{
+		_putchar(s[len]);
+		len--;
+	}
+	_putchar(10);
+}
