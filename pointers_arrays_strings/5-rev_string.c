bdc8aba6747a6cbd973151985845c04f3e40f179
Flagelo6482 <frankln6482@gmail.com>
2023-03-03 10:42:04 -0800

    feat: Function that reverses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *rev_string - Function that reverses a string
+ *@s: Pointer
+ */
+void rev_string(char *s)
+{
+	int len, i;
+
+	char tmp;
+
+	len = 0;
+	i = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+		i++;
+	}
+
+	for (i = len - 1; i >= largo / 2; i--)
+	{
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
+	}
+}
