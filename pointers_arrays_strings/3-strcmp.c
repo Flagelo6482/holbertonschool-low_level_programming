e1d2d14443c4b7c87c71ec355f47a936470c6709
Flagelo6482 <frankln6482@gmail.com>
2023-03-05 09:02:51 -0800

    feat: Update

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,24 +7,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len1, len2;
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (0);
 
-	len1 = 0;
+		s1++;
+		s2++;
+	}
 
-	len2 = 0;
-
-	while (s1[len1] != '\0')
-		len1++;
-
-	while (s2[len2] != '\0')
-		len2++;
-
-	if (len1 == len2)
-		return (0);
-
-	else if (len1 < len2)
-		return (-15);
-
-	else
-		return (15);
+	return (*s1 - *s2);
 }
