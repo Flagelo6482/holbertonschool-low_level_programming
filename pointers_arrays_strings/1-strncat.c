07636e5f5c0ef8fa39b620a2baa0dc275cb221cb
Flagelo6482 <frankln6482@gmail.com>
2023-03-05 05:42:08 -0800

    feat: Update

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -15,11 +15,13 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[len])
 		len++;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[len] = src[i];
 		len++;
 	}
-	
+
+	dest[len] = '\0';
+
 	return (dest);
 }
