e324b4ef32b12ab676b1977c572a10aff41b523c
Flagelo6482 <frankln6482@gmail.com>
2023-03-13 17:44:48 -0700

    feat: Update

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,20 +15,20 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int len1, len2, i;
 
-	len1 = 0;
-	len2 = 0;
 	i = 0;
 
 	if (s1 == NULL || s2 == NULL)
 		s1 = s2 = "";
 
-	while (s1[len1] != '\0')
-		len1++;
+	for (len1 = 0; len1 <= *s1; len1++)
+	{
+	}
 
-	while (s2[len2] != '\0')
-		len2++;
+	for (len2 = 0; len2 <= *s2; len2++)
+	{
+	}
 
-	array = malloc(sizeof(*array) * (len1 + len2 + 1));
+	array = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (array == NULL)
 		return (NULL);
