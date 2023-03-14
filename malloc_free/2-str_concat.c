5d53743adada7f765152147753fe11b1f981d9bb
Flagelo6482 <frankln6482@gmail.com>
2023-03-13 18:45:49 -0700

    feat: Update

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -20,7 +20,7 @@ char *str_concat(char *s1, char *s2)
 	i = 0;
 
 	if (s1 == NULL || s2 == NULL)
-	  s1 = s2 = "";
+		s1 = s2 = "";
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -46,6 +46,6 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	}
 	array[len1] = '\0';
-	
+
 	return (array);
 }
