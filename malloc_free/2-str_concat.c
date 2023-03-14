8c440d4bf169812e7de618ec5ceb01b002668dba
Flagelo6482 <frankln6482@gmail.com>
2023-03-14 08:09:52 -0700

    feat: Update

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,37 +15,36 @@ char *str_concat(char *s1, char *s2)
 	char *array;
 	int len1, len2, i;
 
-	len1 = 0;
-	len2 = 0;
-	i = 0;
+	if (s1 == NULL && s2 == NULL)
+		return ("");
 
-	if (s1 == NULL || s2 == NULL)
-		s1 = s2 = "";
+	else if (s1 == NULL)
+		return (s2);
 
-	while (s1[len1] != '\0')
-		len1++;
+	else if (s2 == NULL)
+		return (s1);
 
-	while (s2[len2] != '\0')
-		len2++;
-
-	array = malloc(sizeof(char) * (len1 + len2 + 1));
-
-	if (array == NULL)
-		return (NULL);
-
-	while (s1[i] != '\0')
+	else
 	{
-		array[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		array[len1] = s2[i];
-		len1++;
-		i++;
-	}
-	array[len1] = '\0';
+		len1 = 0;
+		while (s1[len1] != '\0')
+			len1++;
 
+		len2 = 0;
+		while (s2[len2] != '\0')
+			len2++;
+
+		array = malloc(sizeof(char) * (len1 + len2 + 1));
+
+		for (i = 0; i < len1; i++)
+			array[len1] = s1[i];
+
+		for (i = 0; i < len2; i++)
+		{
+			array[len] = s2[i];
+			len1++;
+		}
+		array[len1] = '\0';
+	}
 	return (array);
 }
