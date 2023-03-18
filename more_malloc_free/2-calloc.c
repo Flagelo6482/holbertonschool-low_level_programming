9e1c5aa0bbb50222d93c2e88f58856bba988db29
Flagelo6482 <frankln6482@gmail.com>
2023-03-17 17:36:54 -0700

    feat: Update

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,17 +14,20 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
 	void *p;
+	char *x;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(size * nmemb);
+	p = malloc((sizeof)size * nmemb);
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		p[i] = 0;
+	x = p;
+
+	for (i = 0; i < (size * nmemb); i++)
+		x[i] = 0;
 
 	return (p);
 }
