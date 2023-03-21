6bd5feece63065bbb284c740ccd9d5e81601a7a1
Flagelo6482 <frankln6482@gmail.com>
2023-03-21 14:41:07 -0700

    feat: Function that prints a struct dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *print_dog - Function that prints a struct dog
+ *@d: Pointer
+ *
+ *Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
