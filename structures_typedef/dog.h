daf0711c7b2343e31230e36cdc2366ca26e4a4aa
Flagelo6482 <frankln6482@gmail.com>
2023-03-21 07:14:56 -0700

    feat: First structure created

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - New type of structure
+ *@name: Char
+ *@age: Float
+ *@owner: char
+ *
+ *Return: Nothing
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
