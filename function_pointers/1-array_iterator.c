6101a20b37b6db250adebf3de5fee8946d713ff7
Flagelo6482 <frankln6482@gmail.com>
2023-03-23 18:37:26 -0700

    feat: Update

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -19,13 +19,13 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 
 	i = 0;
 
-	if (array[i] == '\0')
-		return;
+	/*if (array[i] == '\0')
+	  return;*/
 
 	if (*action == NULL)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (; i < size; i++)
 	{
 		(*action)(array[i]);
 	}
