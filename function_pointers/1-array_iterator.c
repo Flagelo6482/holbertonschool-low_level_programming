d87e07356f9511dff60ade6f15b0c77fe7a25ab9
Flagelo6482 <frankln6482@gmail.com>
2023-03-23 17:54:36 -0700

    feat: Update

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  *array_iterator - Function that executes a function given as a parameter on
@@ -15,6 +16,14 @@
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned long int i;
+
+	i = 0;
+
+	if (array[i] == '\0')
+		return;
+
+	if (*action == NULL)
+		return;
 
 	for (i = 0; i < size; i++)
 	{
