1ff6c5c6b2e0b48d4f0e17b51e4c64cd50bb76a9
Flagelo6482 <frankln6482@gmail.com>
2023-03-25 17:23:28 -0700

    feat: Update

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-#include "3-calc.h"
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
  * main - Principal function
@@ -15,15 +15,31 @@ int main(int argc, char *argv[])
 	int a, b, calc;
 	char *operator;
 
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	operator = argv[2];
+
 	if (argc != 4)
 	{
 		printf("ERROR\n");
 		return (98);
 	}
 
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-	operator = argv[2];
+	if ((argv[2][1]) != '\0')
+	{
+		printf("ERROR\n");
+		return (99);
+	}
 
+	if ((operator == '/' || operator == '%') && (b == 0))
+	{
+		printf("ERROR\n");
+		return (100);
+	}
 
+	calc = (*get_op_func(operator))(a, b);
+
+	printf("%d\n", calc);
+
+	return (0);
 }
