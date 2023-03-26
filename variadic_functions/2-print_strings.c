84b299d0db51ea8786d4d557367f5023e9656626
Flagelo6482 <frankln6482@gmail.com>
2023-03-26 07:40:21 -0700

    feat: Update

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,8 +25,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		if (name == NULL)
 			printf("(nil)");
-
-		printf("%s", name);
+		else
+			printf("%s", name);
 
 		if (i == n - 1)
 			break;
