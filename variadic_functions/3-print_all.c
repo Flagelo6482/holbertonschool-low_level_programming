1f1329fff96b3e82fccf78cb3c6321d19859a21a
Flagelo6482 <frankln6482@gmail.com>
2023-03-28 16:53:57 -0700

    feat: Function that prints anything

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Function that prints anything
+ *@format: Const char *
+ *
+ *Return: Void
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list(param);
+	int i = 0;
+	char *str, *space;
+
+	va_start(param, format);
+
+	if (format)
+	{
+		while (format)
+		{
+			while (format)
+			{
+				switch (format[i])
+				{
+				case 'c':
+					printf("%s%c", space, va_arg(param, int));
+					break;
+				case 'i':
+					printf("%s%d", space, va_arg(param, int));
+					break;
+				case 'f':
+					printf("%s%f", space, va_arg(param, double));
+					break;
+				case 's':
+					str = va_arg(param, char *);
+					if (!str)
+						str = "(nil)";
+
+					printf("%s%s", space, str);
+					break;
+				default:
+					i++;
+					continue;
+				}
+				space = ", ";
+				i++;
+			}
+		}
+	}
+	printf("\n");
+	va_end(param);
+}
