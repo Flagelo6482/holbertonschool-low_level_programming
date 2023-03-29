b7dca9229c57d50e7e873f5e11545259aab25ec2
Flagelo6482 <frankln6482@gmail.com>
2023-03-28 17:17:21 -0700

    feat: Update

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -21,33 +21,30 @@ void print_all(const char * const format, ...)
 	{
 		while (format)
 		{
-			while (format)
+			switch (format[i])
 			{
-				switch (format[i])
-				{
-				case 'c':
-					printf("%s%c", space, va_arg(param, int));
-					break;
-				case 'i':
-					printf("%s%d", space, va_arg(param, int));
-					break;
-				case 'f':
-					printf("%s%f", space, va_arg(param, double));
-					break;
-				case 's':
-					str = va_arg(param, char *);
-					if (!str)
-						str = "(nil)";
+			case 'c':
+				printf("%s%c", space, va_arg(param, int));
+				break;
+			case 'i':
+				printf("%s%d", space, va_arg(param, int));
+				break;
+			case 'f':
+				printf("%s%f", space, va_arg(param, double));
+				break;
+			case 's':
+				str = va_arg(param, char *);
+				if (!str)
+					str = "(nil)";
 
-					printf("%s%s", space, str);
-					break;
-				default:
-					i++;
-					continue;
-				}
-				space = ", ";
+				printf("%s%s", space, str);
+				break;
+			default:
 				i++;
+				continue;
 			}
+			space = ", ";
+			i++;
 		}
 	}
 	printf("\n");
