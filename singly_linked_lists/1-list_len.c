18df36ada758cb67f8c8a193cfa1b53682eecac2
Flagelo6482 <frankln6482@gmail.com>
2023-04-06 06:59:33 -0700

    feat: Main function :rocket:

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * list_len - Function that returns the number of elements
+ *            linked list
+ *@h: Pointer to struct
+ *
+ *Return: Number of elements in a linked list
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL && h->len != NULL)
+			count++;
+
+		h = h->next;
+	}
+
+	return (count);
+}
