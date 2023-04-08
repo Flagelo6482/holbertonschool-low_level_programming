e7dd6dfa7c398d5da771759a648afe30f66bdec1
Flagelo6482 <frankln6482@gmail.com>
2023-04-08 06:55:02 -0700

    feat: Principal function :rocket:

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * dlistint_len - Function that returns the number of elements in a linked list
+ *
+ *@h: Pointer to list
+ *
+ *Return: Number of elements
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while(h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
