c1293899fe8a968e5efef8deb24aefb13e87d622
Flagelo6482 <frankln6482@gmail.com>
2023-04-08 19:58:32 -0700

    feat: Principal function :rocket:

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * sum_dlistint - Function that returns the sum of all the data (n) of a list
+ *
+ *@head: Pointer to list
+ *
+ *Return: Sum of all the data list
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	dlistint_t *tmp = head;
+
+	if (tmp == NULL)
+		return (sum);
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		sum += tmp->n;
+	}
+
+	return (sum);
+}
