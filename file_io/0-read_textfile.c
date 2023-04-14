889fa014cc04f6ac824798d60b027e8b86e1b3ae
Flagelo6482 <frankln6482@gmail.com>
2023-04-14 07:23:37 -0700

    feat: Update :bug:

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
  * read_textfile - Function that reads a text file and prints
