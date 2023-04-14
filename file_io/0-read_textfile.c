9b85396e01708c331dc4b024b7d4355f39d316e9
Flagelo6482 <frankln6482@gmail.com>
2023-04-14 07:04:44 -0700

    feat: Principal function :rocket:

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Function that reads a text file and prints
+ *@filename: File
+ *@letters: Number of letters
+ *
+ *Return: Number of letters in a file
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int total, fd;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	total = read(fd, buffer, letters);
+
+	write(STDIN_FILENO, buffer, total);
+
+	close(fd);
+
+	return (total);
+}
