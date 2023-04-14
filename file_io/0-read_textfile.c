74ad5465d3a9f72a4b336e4b103d5c60f2897bc6
Flagelo6482 <frankln6482@gmail.com>
2023-04-14 07:22:29 -0700

    feat: Update :bug:

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,8 +1,5 @@
-#include <unistd.h>
 #include "main.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - Function that reads a text file and prints
@@ -20,18 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
-	{
-		close(fd);
 		return (0);
-	}
+
+	fd = open(filename, O_RDONLY);
 
 	total = read(fd, buffer, letters);
 
