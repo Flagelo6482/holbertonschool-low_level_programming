6b79403c9b7d46d4a6aae52bc07859ac8119e429
Flagelo6482 <frankln6482@gmail.com>
2023-04-14 07:11:49 -0700

    feat: Update :bug:

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -20,17 +20,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-	{
-		return (0);
-	}
-
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
 	total = read(fd, buffer, letters);
 
