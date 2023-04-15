b2948a35770f67aacd87502402a2fea0ee0460ab
Flagelo6482 <frankln6482@gmail.com>
2023-04-15 15:34:39 -0700

    feat: Principal function :rocket:

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_create - Function that creates a hash table
+ *
+ *@size: Parameter
+ *
+ *Return: Hash table address
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *nuevo = malloc(sizeof(hash_table_t));
+
+	if (nuevo == NULL)
+		return (NULL);
+
+	nuevo->size = size;
+	nuevo->array = NULL;
+
+	return (nuevo);
+}
