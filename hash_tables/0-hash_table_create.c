9420ca0edbf8996025046d6bdccb0aefd54d32b9
Flagelo6482 <frankln6482@gmail.com>
2023-04-15 15:44:53 -0700

    feat: Update :bug:

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,13 +11,20 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *nuevo = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *nuevo = malloc(sizeof(hash_table_t));
+	unsigned long int i;
 
 	if (nuevo == NULL)
 		return (NULL);
 
 	nuevo->size = size;
-	nuevo->array = NULL;
+	nuevo->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (nuevo->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		nuevo->array = NULL;
 
 	return (nuevo);
 }
