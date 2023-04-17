1d35eea2070d387a7696a9f4cc9ce470805e53a0
Flagelo6482 <frankln6482@gmail.com>
2023-04-17 16:33:44 -0700

    feat: Principal function :rocket:

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,40 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Function that prints a hash table
+ *@ht: Pointer to hash table
+ *
+ *Return: Void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	unsigned long int i;
+	unsigned char flag = 0;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+
+			tmp = ht->array[i];
+
+			while (tmp != NULL)
+			{
+				printf("'%s: '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+
+				if (tmp != NULL)
+					printf(", ");
+			}
+			flag = 1;
+		}
+	}
+	printf("}\n");
+}
