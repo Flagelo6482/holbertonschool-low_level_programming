577aca544c92f29a5c0f34bec037b357e07d34f0
Flagelo6482 <frankln6482@gmail.com>
2023-04-17 15:28:52 -0700

    feat: Update :bug:

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -24,8 +24,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	tmp = ht->array[index];
 
+	while (tmp && strcmp(tmp->key, key) != 0)
+		tmp = tmp->next;
+
 	if ((*tmp->key) == (*key))
 		return (tmp->value);
 	else
-		return("(null)");
+		return ("(null)");
 }
