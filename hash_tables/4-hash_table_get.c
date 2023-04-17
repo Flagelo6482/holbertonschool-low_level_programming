aa65bab83c7b9a1aa9f255030bac6d6ecc59e35c
Flagelo6482 <frankln6482@gmail.com>
2023-04-17 15:25:36 -0700

    feat: Update :bug:

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,18 +14,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *tmp;
 	unsigned long int index;
 
-	index = key_index((const unsigned char *)key, ht->size);
-
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
-	if (index >= ht->size)
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if (ht->array[index] == NULL)
 		return (NULL);
 
 	tmp = ht->array[index];
 
-	while (tmp && strcmp(tmp->key, key) != 0)
-		tmp = tmp->next;
-
-	return ((tmp == NULL) ? NULL : tmp->value);
+	if ((*tmp->key) == (*key))
+		return (tmp->value);
+	else
+		return("(null)");
 }
