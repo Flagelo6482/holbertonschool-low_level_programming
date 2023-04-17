e9e9a53631a6aa27154d887fb89a4362b5fe7a5c
Flagelo6482 <frankln6482@gmail.com>
2023-04-17 16:58:47 -0700

    feat: Update :bug:

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -29,7 +29,7 @@ void hash_table_print(const hash_table_t *ht)
 
 				while (tmp != NULL)
 				{
-					printf("'%s: '%s'", tmp->key, tmp->value);
+					printf("'%s': '%s'", tmp->key, tmp->value);
 					tmp = tmp->next;
 
 					if (tmp != NULL)
